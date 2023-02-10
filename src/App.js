088319c391a8a74c1f7efcba75f0b1a857b53f6d
LaveeshkaMahadea <laveeshka94@gmail.com>
2023-02-10 23:40:13 +1100

    lift fetch subscriptions data to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import './App.css';
 import Router from "./routes/routes.js";
+// useEffect
+import { useEffect } from "react";
+// redux
+import { useDispatch, useSelector } from "react-redux";
+import { getSubscriptions } from "./redux/subscriptionsSlice";
+
 
 function App() {
+
+  //retrieve state and actions from store
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+  const token = useSelector((state) => state.user.token);
+  let subscriptions = useSelector((state) => state.subscriptions.subscriptions);
+  console.log("Subscriptions are: ", subscriptions)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isLoggedIn){
+      try {
+        const resultAction = dispatch(getSubscriptions(token)).unwrap();
+        //console.log("Fetched subscriptions data in Subscriptions page is: ", resultAction);
+      } catch (err) {
+        console.warn(err);
+      }
+    }
+  }, [isLoggedIn])
 
   return (
     <div className="App">
--- src/pages/Subscriptions.jsx
+++ src/pages/Subscriptions.jsx
@@ -12,22 +12,22 @@ export default function Subscriptions() {
 
   //retrieve state and actions from store
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-  const token = useSelector((state) => state.user.token);
-  let subscriptions = useSelector((state) => state.subscriptions.subscriptions);
-  console.log("Subscriptions are: ", subscriptions)
-  const dispatch = useDispatch();
+//   const token = useSelector((state) => state.user.token);
+    let subscriptions = useSelector((state) => state.subscriptions.subscriptions);
+//   console.log("Subscriptions are: ", subscriptions)
+//   const dispatch = useDispatch();
 
-  useEffect(() => {
-    try {
-        const resultAction = dispatch(getSubscriptions(token)).unwrap();
-        //console.log("Fetched subscriptions data in Subscriptions page is: ", resultAction);
-      } catch (err) {
-        console.warn(err);
-      }
-  }, [])
+//   useEffect(() => {
+//     try {
+//         const resultAction = dispatch(getSubscriptions(token)).unwrap();
+//         //console.log("Fetched subscriptions data in Subscriptions page is: ", resultAction);
+//       } catch (err) {
+//         console.warn(err);
+//       }
+//   }, [])
 
 
   if (!isLoggedIn) return <Navigate to="/login" />;
 
-  return <div>{subscriptions.length > 0 ? "Yes data" : "No data"}</div>;
+  return <div>{subscriptions.length > 0 ? "Yass data" : "Nope"}</div>;
 }
